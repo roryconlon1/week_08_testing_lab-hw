e5c99a09e08b75c2f666064395d9f31513267722
Rory Conlon <roryconlon1@gmail.com>
2022-08-22 14:50:20 +0100

    cypress

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -7,4 +7,49 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should update display of running total', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-equals').click();
+    cy.get('#running-total').click();
+    cy.get('.display').should('contain', 4)
+  })
+
+  it('should be able to multiply', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('#running-total').click();
+    cy.get('.display').should('contain', 20)
+  })
+
+  it('should be able to divide', () => {
+    cy.get('#number6').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('#running-total').click();
+    cy.get('.display').should('contain', 3)
+  })
+
+  it('should be able to subtract', () => {
+    cy.get('#number6').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('#running-total').click();
+    cy.get('.display').should('contain', 4)
+  })
+
+  it('should be able to add', () => {
+    cy.get('#number6').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('#running-total').click();
+    cy.get('.display').should('contain', 8)
+  })
+
+  it('sgould be able to perform multiple operations')
 })
